109f052dee8f10b387a6cd2653494f98730c59ba
yejuPark <pyj8977@daum.net>
2023-08-24 10:50:10 +0900

    Read

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'index.html', context)
+
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+    context = {
+        'article': article,
+    }
+    return render(request, 'detail.html', context)
